6116c63df00d4b9e6d674ba0a8bb8ae1c6ee76d1
Shamim448 <shamim.448@gmail.com>
2023-07-25 19:06:04 +0600

    validation done

--- Book/BookApp/Controllers/CategoryController.cs
+++ Book/BookApp/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BookApp.Data;
 using BookApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace BookApp.Controllers
 {
@@ -15,5 +16,25 @@ namespace BookApp.Controllers
            List<Category> getAllCatagories = _context.Categories.ToList();
             return View(getAllCatagories);
         }
+        
+        public IActionResult Create() 
+        {
+            return View();
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString()) {
+                ModelState.AddModelError("name", "Name and Display order candot be same");
+            }
+            if(ModelState.IsValid)
+            {
+                _context.Categories.Add(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
     }
 }
--- Book/BookApp/Models/Category.cs
+++ Book/BookApp/Models/Category.cs
@@ -7,9 +7,11 @@ namespace BookApp.Models
     {
       
         public int Id { get; set; }
+        [Required, MaxLength(30)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1, 30)]
         public int DisplayOrder { get; set; }
     }
 }
