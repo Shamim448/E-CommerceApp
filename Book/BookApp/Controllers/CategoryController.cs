bbb6fda1949d49c62e9c79ad4b150e9ef2366b20
Shamim448 <shamim.448@gmail.com>
2023-07-25 16:08:23 +0600

    category view and theme add

--- Book/BookApp/Controllers/CategoryController.cs
+++ Book/BookApp/Controllers/CategoryController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookApp.Data;
+using BookApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookApp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context) {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+           List<Category> getAllCatagories = _context.Categories.ToList();
+            return View(getAllCatagories);
         }
     }
 }
